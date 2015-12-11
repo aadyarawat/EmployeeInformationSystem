7533b69452e3faf3c71b5aaf6bcb5880e5991a49
aadyarawat <aadyarawat4@gmail.com>
2015-12-11 15:22:02 +0530

    EmpInfo

---
+++ src/main/java/com/yash/EmployeeInformation/App.java
@@ -0,0 +1,13 @@
+package com.yash.EmployeeInformation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
