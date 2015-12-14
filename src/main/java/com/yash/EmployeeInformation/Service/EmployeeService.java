0499b812ea93c1ef01b5db2fb3407e0241d9fb53
prakhartkg <prakhar.jain@YITRNG17DT.Yash.com>
2015-12-14 12:10:18 +0530

    Full Proj

---
+++ src/main/java/com/yash/EmployeeInformation/Service/EmployeeService.java
@@ -0,0 +1,20 @@
+package com.yash.EmployeeInformation.Service;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class EmployeeService
+ */
+@Stateless
+@LocalBean
+public class EmployeeService implements EmployeeServiceLocal {
+
+    /**
+     * Default constructor. 
+     */
+    public EmployeeService() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
