02a069cb0dba905c284b02f0418b7e123bca3294
prakhartkg <prakhar.jain@YITRNG17DT.Yash.com>
2015-12-14 15:09:38 +0530

    domain

--- src/main/java/com/yash/EmployeeInformation/domain/Employee.java
+++ src/main/java/com/yash/EmployeeInformation/domain/Employee.java
@@ -1,5 +1,79 @@
 package com.yash.EmployeeInformation.domain;
 
-public class Employee {
+import java.util.List;
 
+public class Employee {
+	private int employeeId;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String address;
+	private String state;
+	private String city;
+	private String mobile;
+	private String alternate_mobile;
+	private List<Project> projects;
+	 
+	public int getEmployeeId() {
+	return employeeId;
+	}
+	public void setEmployeeId(int employeeId) {
+	this.employeeId = employeeId;
+	}
+	public String getFirstName() {
+	return firstName;
+	}
+	public void setFirstName(String firstName) {
+	this.firstName = firstName;
+	}
+	public String getLastName() {
+	return lastName;
+	}
+	public void setLastName(String lastName) {
+	this.lastName = lastName;
+	}
+	public String getEmail() {
+	return email;
+	}
+	public void setEmail(String email) {
+	this.email = email;
+	}
+	public String getAddress() {
+	return address;
+	}
+	public void setAddress(String address) {
+	this.address = address;
+	}
+	public String getState() {
+	return state;
+	}
+	public void setState(String state) {
+	this.state = state;
+	}
+	public String getCity() {
+	return city;
+	}
+	public void setCity(String city) {
+	this.city = city;
+	}
+	public String getMobile() {
+	return mobile;
+	}
+	public void setMobile(String mobile) {
+	this.mobile = mobile;
+	}
+	public String getAlternate_mobile() {
+	return alternate_mobile;
+	}
+	public void setAlternate_mobile(String alternate_mobile) {
+	this.alternate_mobile = alternate_mobile;
+	}
+	public List<Project> getProjects() {
+		return projects;
+	}
+	public void setProjects(List<Project> projects) {
+		this.projects = projects;
+	}
+	
+	
 }
