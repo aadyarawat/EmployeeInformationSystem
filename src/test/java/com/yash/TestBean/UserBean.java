237d43eb552b19c34edf03d9f4a0f673d395c658
ramsomani <ram.somani36@gmail.com>
2015-12-15 11:16:38 +0530

    LDAP

---
+++ src/test/java/com/yash/TestBean/UserBean.java
@@ -0,0 +1,91 @@
+package com.yash.TestBean;
+
+import java.util.Hashtable;
+
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+import javax.naming.Context;
+import javax.naming.directory.DirContext;
+import javax.naming.directory.InitialDirContext;
+import javax.servlet.http.HttpSession;
+
+
+
+@ManagedBean
+@SessionScoped
+public class UserBean {
+	private Boolean check;
+	private String name;
+	private String password;
+	
+	FacesContext facesContext = FacesContext.getCurrentInstance();
+	HttpSession session = (HttpSession) facesContext.getExternalContext().getSession(true);
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String authenticate(){
+	check=getConnection(name, password);
+	if(check==false){
+		return "index.xhtml?faces-redirect=true&error=Invalid UserName And Password";
+	}
+	else{	
+		session.setAttribute("SessionEmail", name);
+		return "Welcome.xhtml";
+	}
+	
+	}
+	
+	
+	public boolean getConnection(String ldapUsername,String ldapPassword )
+	{
+			
+		final  String ldapAdServer ="ldap://inidradc01.yash.com/";
+       
+		 Hashtable<String, Object> env = new    Hashtable<String, Object>();
+	       
+        env.put(Context.SECURITY_AUTHENTICATION, "simple");
+        
+        if(ldapUsername != null) 
+        {
+            env.put(Context.SECURITY_PRINCIPAL, ldapUsername);
+        }
+        if(ldapPassword != null) 
+        {
+            env.put(Context.SECURITY_CREDENTIALS, ldapPassword);
+        }
+        
+        env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
+        env.put(Context.PROVIDER_URL, ldapAdServer);
+
+        env.put("java.naming.ldap.attributes.binary", "objectSID");
+        env.put(Context.REFERRAL,"follow");
+        
+       try
+       {
+    	   @SuppressWarnings("unused")
+    	   DirContext ctx = new InitialDirContext(env);
+
+    	   return true;
+       }
+       catch(Exception e)
+       {
+
+    	   return false;
+    	   
+       }
+	}
+	
+}
