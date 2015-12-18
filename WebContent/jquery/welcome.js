b8522ab4774a736c47fe73b82cdcfdfbb25a884a
adityamoghe <adityamoghe30@gmail.com>
2015-12-18 10:59:08 +0530

    by adi minor changes

--- WebContent/jquery/welcome.js
+++ WebContent/jquery/welcome.js
@@ -3,11 +3,11 @@
  */
 
 $(function() {
-	
-	
-	
+
 	$("#accordion").accordion({
 		collapsible : true,
+		autoHeight : false,
+		navigation : true,
 
 		beforeActivate : function(event, ui) {
 			ui.newHeader.add(ui.newPanel).prependTo(this)
